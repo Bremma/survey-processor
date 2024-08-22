0f7c7b3eb22e3a980ac76640e0080fd188491e0c
Heather Selbe <hselbe@vt.edu>
2024-08-21 21:34:23 -0400

    Update survey_processor.sh
    * redid gdown command to not rely on version 4.6.0 of GDOWN (tested on 5.2.0)
    * also removed logic that would change open? to uc? in urls (hold over from gdown 6.4.0
    *added comments so I remeber what I did

--- survey_processor.sh
+++ survey_processor.sh
@@ -1,15 +1,29 @@
 #!/bin/bash
 
-#Process:
+#Input:
+#   * CSV of names in col2 and gdrive URLs in col3
+#Output: 
+#   * text file of names in col2, formatted for text slide show in OBS
+#   * pull.sh to pull down all N images, numbering them 3 to N+3
 
-# 1. Chunk file into comma separated cat names and picture list
-# 2. Print Cat names into a file
-# 3. Download images from google and rename with numbers
+#Clean up old files that we will be generating
 rm -f names pull.sh
 rm -f processed/*
+#create folder for images
 mkdir processed
-sed s/open?/uc?/ $1 > processed.csv
+#Hold over from doing some modifications, also so we don't risk junking the orignal file
+cp $1 processed.csv
+#File count start offset
 COUNTER=3
+
+#While processed.csv has lines,
+# print name and url for visibility
+# print names on a line with 2 empty lines for separation
+# generate pull.sh with the commands:
+#   gdown --fuzzy <raw gdrive link> (returns file named <filename> - <submitter name>
+#   EXT=`file *-*| awk -F':' '{ print $1 }' | awk -F'.' '{ print $NF }'` (sets EXT env variable to file's extension)
+#   mv *.$EXT processed/<COUNTER>.$EXT
+
 
 while IFS=',' read -r col1 col2 col3
 do
@@ -17,7 +31,7 @@ do
    echo $col2 | tr -d '"' >> names
    echo "" >> names
    echo "" >> names
-   echo gdown $col3 >> pull.sh
+   echo gdown --fuzzy $col3 >> pull.sh
    OUT="EXT=\`file *-*| awk -F':' '{ print \$1 }' | awk -F'.' '{ print \$NF }'\`"
    echo $OUT >> pull.sh
    OUT="\$EXT"
@@ -25,8 +39,13 @@ do
    let COUNTER++
 done < processed.csv
 
+# convert pull.sh to UNIX format (Cygwin!)
+# Run pull.sh
+# Returns all images from CSV in the form of N.EXT
 dos2unix pull.sh
 chmod +x pull.sh
 ./pull.sh
+#Cleanup processed.csv
 rm -f processed.csv
+
 
